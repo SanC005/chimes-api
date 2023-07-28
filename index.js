088052f8e7d987c42a7f76fb8ed4d4a5b986a4ce
SanC005 <sandeepcsrao@gmail.com>
2023-07-28 12:37:31 +0530

    intialized project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 8080;
+app.listen( PORT, () => console.log(`working on http://localhost:${PORT}`))
+app.get('/profile', (req,res) => {
+    res.status(200).send({
+        user: "sandeep",
+        logo: '⚽️'
+    })
+})
