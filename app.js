3ee23d43964c88b1c71a9bea5344d7f6d5593e1c
SanC005 <sandeepcsrao@gmail.com>
2023-07-29 15:46:08 +0530

    database connected improved folder structure

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = 5001
 const posts = require('./routes/posts')
+const connectDB = require('./database/connect')
+require('dotenv').config()
+
 
 //middleware 
 app.use(express.json())
@@ -14,4 +17,12 @@ app.get('/hello', (req,res) => {
 
 app.use('/api/v1/posts',posts)
 
-app.listen(port , console.log(`Server is listening on http://localhost/${port}...`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port , console.log(`Server is listening on http://localhost/${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start();
