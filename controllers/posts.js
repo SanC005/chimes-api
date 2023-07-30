2411c9c37d038e0e5dfa7b8d42d4e3512a8cbb61
SanC005 <sandeepcsrao@gmail.com>
2023-07-31 00:13:35 +0530

    added getOne post and delete post functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,21 +1,52 @@
-const Post = require('../model/postModel').default
+const Post = require('../model/postModel')
 
-const getAllPosts = (req,res) => {
-    res.send("getting all posts")
+const getAllPosts = async(req,res) => {
+    try {
+        const posts = await Post.find({})
+        res.status(201).json({posts})
+
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const addPost = async(req,res) => {
     // res.json(req.body)
-    const post = await Post.create(req.body)
+    try{
+        const post = await Post.create(req.body)
     res.status(201).send({post})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+    
 }
-const getPost = (req,res) => {
-    res.json({id: req.params.id})
+const getPost = async(req,res) => {
+    try {
+        const {id:PostID} = req.params
+        const post = await Post.findOne({_id:PostID})
+        if(!post){
+            return res.status(404).json({msg:`No post found with id : ${PostID}`})
+        }
+        res.status(201).json({post})
+
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const updatePost = (req,res) => {
     res.send("update a post")
 }
-const deletePost = (req,res) => {
-    res.send("delete a post")
+const deletePost = async(req,res) => {
+    try {
+        const {id:PostID} = req.params
+        const post = await Post.findOneAndDelete({_id:PostID})
+        if(!post){
+            return res.status(404).json({msg:`No post found with id : ${PostID}`})
+        }
+        res.status(201).json({post})
+
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 
--- model/postModel.js
+++ model/postModel.js
@@ -1,15 +1,36 @@
 const mongoose = require('mongoose');
 // import { Schema, model } from 'mongoose';
 
-const postSchema = new Schema({
-    id: {
-        type:Number,
-        required:[true,'id si required field']
+const postSchema = new mongoose.Schema({
+    // id: {
+    //     type:Number,
+    //     required:[true,'id is required field'],
+    //     trim:true,
+    // },
+    title: {
+        type:String,
+        // required:[true,'id is required field'],
+        default: "no title",
+        trim:true,
     },
-    title: String,
-    img: String,
-    postTitle: String,
-    link: String
+    img: {
+        type:String,
+        // required:[true,'id is required field'],
+        default: "https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg",
+        trim:true,
+    },
+    postTitle: {
+        type:String,
+        // required:[true,'id is required field'],
+        default: "no title",
+        trim:true,
+    },
+    link: {
+        type:String,
+        required:[true,'link is required field'],
+        trim:true,
+    },
+    
 })
 
 module.exports = mongoose.model('Post',postSchema);
