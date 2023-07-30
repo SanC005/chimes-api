fc83c5a6d71a10de5f2fd3dc365f471aa1a8f831
SanC005 <sandeepcsrao@gmail.com>
2023-07-31 01:40:45 +0530

    added notfound msg

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const posts = require('./routes/posts')
 const connectDB = require('./database/connect')
 const path = require('path')
 require('dotenv').config()
-
+const NotFound = require('./middleware/not-found')
 //middleware 
-app.use(express.static(path.join(__dirname, '/static')))
+app.use(express.static(path.join(__dirname, '../src')))
 app.use(express.json())
 
 //routes
@@ -17,6 +17,7 @@ app.use(express.json())
 // })
 
 app.use('/api/v1/posts',posts)
+app.use(NotFound)
 
 const start = async () => {
     try {
